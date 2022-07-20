25286fe71ac038f063dd835812bfd8f7c24bfaef
VitalyIgorevich <Vitaly.Igorevich.D@gmail.com>
2022-07-20 21:20:21 +0400

    menu category functionality

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,16 +4,17 @@ const Categories = () => {
   const[activeCategories, setActiveCategories] = React.useState(0)
 
   const categories = ["Все","Мясные","Вегетарианская","Гриль", "Острые", "Закрытые"]
+
+  const makeActive = (index) => {
+    setActiveCategories(index)
+  }
   
   return (
     <div class="categories">
     <ul>
-      <li className="active" >Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
+      {categories.map((value,i) => 
+        <li onClick={() =>makeActive(i)} className={activeCategories === i ? "active" : ''} >{value}</li>
+      )}
     </ul>
   </div>
 )}
